8c8526af78ac414b13b85f3af7a1bbbe8d590b64
raihangh <raihanghassani7@gmail.com>
2022-04-01 15:15:36 -0700

    styling css

--- mod2/src/components/musicbox/musicbox.js
+++ mod2/src/components/musicbox/musicbox.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './musicbox.css'
 
-const musicbox = ({title, artist, urlimg, urlspotify}) => {
+export default function Track({ urlimg, title, artist, urlspotify, toggleSelect }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleToggleSelect = () => {
+    setIsSelected(!isSelected);
+    toggleSelect();
+  }
+
   return (
     <div className='musicbox-wrap'>
-        <div className="music-content">
-            <img className='img-song' src={urlimg} alt={title} />
-            <p className='title-song'>{title}</p>
-            <p className='artis-song'>{artist}</p>
-        </div> 
-        <a className='btn btn-blue' href={urlspotify}>click</a>
-    </div>
-  )
+         <div className="music-content">
+             <img className='img-song' src={urlimg} alt={title} />
+             <p className='title-song'>{title}</p>
+             <p className='artis-song'>{artist}</p>
+         </div> 
+         
+         <div className="music-action">
+          <a className='btn btn-spotify' href={urlspotify}>Play</a>
+          <a className='btn' onClick={handleToggleSelect}>{isSelected ? 'Deselect' : 'Select'}</a>
+        </div>
+     </div>
+  );
 }
 
-export default musicbox
+// const musicbox = ({title, artist, urlimg, urlspotify}) => {
+//   return (
+//     <div className='musicbox-wrap'>
+//         <div className="music-content">
+//             <img className='img-song' src={urlimg} alt={title} />
+//             <p className='title-song'>{title}</p>
+//             <p className='artis-song'>{artist}</p>
+//         </div> 
+//         <a className='btn btn-blue' href={urlspotify}>click</a>
+//     </div>
+//   )
+// }
+
+// export default musicbox
--- mod2/src/pages/home.js
+++ mod2/src/pages/home.js
@@ -1,92 +1,196 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Musicbox from '../components/musicbox/musicbox';
 import SearchBar from '../components/searchbar/index';
 import config from '../lib/config';
 
-export default class Home extends Component {
-  state = {
-    accessToken: '',
-    isAuthorize: false,
-    tracks: [],
-  }
+export default function Home() {
+  const [accessToken, setAccessToken] = useState('');
+  const [isAuthorize, setIsAuthorize] = useState(false);
+  const [tracks, setTracks] = useState([]);
+  const [selectedTracksUri, setSelectedTracksUri] = useState([]);
+  const [isInSearch, setIsInSearch] = useState(false);
 
-  getHashParams() {
-    const hashParams = {};
-    const r = /([^&;=]+)=?([^&;]*)/g;
-    const q = window.location.hash.substring(1);
-    let e = r.exec(q);
+  useEffect(() => {
+    const accessToken = new URLSearchParams(window.location.hash).get('#access_token');
 
-    while (e) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-      e = r.exec(q);
+    setAccessToken(accessToken);
+    setIsAuthorize(accessToken !== null);
+  }, []);
+
+  useEffect(() => {
+    if (!isInSearch) {
+      const selectedTracks = filterSelectedTracks();
+
+      setTracks(selectedTracks);
     }
-    return hashParams;
-  }
+  }, [selectedTracksUri]);
 
-  componentDidMount() {
-    const params = this.getHashParams();
-    const { access_token: accessToken } = params;
-
-    this.setState({ accessToken, isAuthorize: accessToken !== undefined })
-  }
-
-  getSpotifyLinkAuthorize() {
+  const getSpotifyLinkAuthorize = () => {
     const state = Date.now().toString();
     const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
     return `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:3000&state=${state}&scope=${config.SPOTIFY_SCOPE}`;
   }
 
-  onSuccessSearch(tracks) {
-    this.setState({ tracks });
+  const filterSelectedTracks = () => {
+    return tracks.filter((track) => selectedTracksUri.includes(track.uri));
   }
 
-  render() {
-    return (
-      <div className='container' style={{
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        justifyContent: 'center',
-      }}>
-        {!this.state.isAuthorize && (
-          <main className="center">
-            <p>Login for next step...</p>
-            <a className='btn' href={this.getSpotifyLinkAuthorize()}>Authorize</a>
-          </main>
-        )}
+  const onSuccessSearch = (searchTracks) => {
+    setIsInSearch(true);
+    const selectedTracks = filterSelectedTracks();
+    const searchDistincTracks = searchTracks.filter((track) => !selectedTracksUri.includes(track.uri));
 
-        {this.state.isAuthorize && (
-          <main className="container" id="home">
-            <SearchBar
-              accessToken={this.state.accessToken}
-              onSuccess={(tracks) => this.onSuccessSearch(tracks)}
-            />
-
-            <div className="content">
-              {this.state.tracks.length === 0 && (
-                <p>No tracks</p>
-              )}
-
-              <div className="cards" style={{
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: 'center',
-                }}>
-                {this.state.tracks.map((song) => (
-                  <Musicbox
-                    key={song.id}
-                    urlimg={song.album.images[0].url}
-                    title={song.name}
-                    artist={song.artists[0].name}
-                    urlspotify={song.external_urls.spotify}
-                  />
-                ))}
-              </div>
-            </div>
-          </main>
-        )}
-      </div>
-    );
+    setTracks([...selectedTracks, ...searchDistincTracks]);
   }
+
+
+  const clearSearch = () => {
+    const selectedTracks = filterSelectedTracks();
+    
+    setTracks(selectedTracks);
+    setIsInSearch(false);
+  }
+
+
+  const toggleSelect = (track) => {
+    const uri = track.uri;
+
+    if (selectedTracksUri.includes(uri)) {
+      setSelectedTracksUri(selectedTracksUri.filter((item) => item !== uri));
+    } else {
+      setSelectedTracksUri([...selectedTracksUri, uri]);
+    }
+  }
+
+  return (
+    <>
+      {!isAuthorize && (
+        <main className="center">
+          <p>Login for next step...</p>
+          <a href={getSpotifyLinkAuthorize()}><button>Authorize</button></a>
+        </main>
+      )}
+
+      {isAuthorize && (
+        <main className="container" id="home">
+          <SearchBar
+            accessToken={accessToken}
+            onSuccess={(tracks) => onSuccessSearch(tracks)}
+            onClearSearch={clearSearch}
+          />
+
+          <div className="content">
+            {tracks.length === 0 && (
+              <p>No tracks</p>
+            )}
+          <div className="cards" style={{
+               display: "flex",
+               flexWrap: "wrap",
+               justifyContent: 'center',
+             }}>
+
+              {tracks.map((track) => (
+                <Musicbox
+                  key={track.id}
+                  urlimg={track.album.images[0].url}
+                  title={track.name}
+                  artist={track.artists[0].name}
+                  urlspotify={track.external_urls.spotify}
+                  toggleSelect={() => toggleSelect(track)}
+                />
+              ))}
+          </div>
+          </div>
+        </main>
+      )}
+    </>
+  );
 }
+// export default class Home extends Component {
+//   state = {
+//     accessToken: '',
+//     isAuthorize: false,
+//     tracks: [],
+//   }
+
+//   getHashParams() {
+//     const hashParams = {};
+//     const r = /([^&;=]+)=?([^&;]*)/g;
+//     const q = window.location.hash.substring(1);
+//     let e = r.exec(q);
+
+//     while (e) {
+//       hashParams[e[1]] = decodeURIComponent(e[2]);
+//       e = r.exec(q);
+//     }
+//     return hashParams;
+//   }
+
+//   componentDidMount() {
+//     const params = this.getHashParams();
+//     const { access_token: accessToken } = params;
+
+//     this.setState({ accessToken, isAuthorize: accessToken !== undefined })
+//   }
+
+//   getSpotifyLinkAuthorize() {
+//     const state = Date.now().toString();
+//     const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+
+//     return `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:3000&state=${state}&scope=${config.SPOTIFY_SCOPE}`;
+//   }
+
+//   onSuccessSearch(tracks) {
+//     this.setState({ tracks });
+//   }
+
+//   render() {
+//     return (
+//       <div className='container' style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         flexWrap: "wrap",
+//         justifyContent: 'center',
+//       }}>
+//         {!this.state.isAuthorize && (
+//           <main className="center">
+//             <p>Login for next step...</p>
+//             <a className='btn' href={this.getSpotifyLinkAuthorize()}>Authorize</a>
+//           </main>
+//         )}
+
+//         {this.state.isAuthorize && (
+//           <main className="container" id="home">
+//             <SearchBar
+//               accessToken={this.state.accessToken}
+//               onSuccess={(tracks) => this.onSuccessSearch(tracks)}
+//             />
+
+//             <div className="content">
+//               {this.state.tracks.length === 0 && (
+//                 <p>No tracks</p>
+//               )}
+
+//               <div className="cards" style={{
+//                   display: "flex",
+//                   flexWrap: "wrap",
+//                   justifyContent: 'center',
+//                 }}>
+//                 {this.state.tracks.map((song) => (
+//                   <Musicbox
+//                     key={song.id}
+//                     urlimg={song.album.images[0].url}
+//                     title={song.name}
+//                     artist={song.artists[0].name}
+//                     urlspotify={song.external_urls.spotify}
+//                   />
+//                 ))}
+//               </div>
+//             </div>
+//           </main>
+//         )}
+//       </div>
+//     );
+//   }
+// }
