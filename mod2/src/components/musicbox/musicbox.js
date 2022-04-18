bb2e934fbc6fe0b9380c8a12c98c5969a2e577cf
raihangh <raihanghassani7@gmail.com>
2022-04-18 15:55:20 +0700

    typescript

--- mod2/src/components/musicbox/musicbox.js
+++ mod2/src/components/musicbox/musicbox.js
@@ -24,18 +24,3 @@ export default function Track({ urlimg, title, artist, urlspotify, toggleSelect,
      </div>
   );
 }
-
-// const musicbox = ({title, artist, urlimg, urlspotify}) => {
-//   return (
-//     <div className='musicbox-wrap'>
-//         <div className="music-content">
-//             <img className='img-song' src={urlimg} alt={title} />
-//             <p className='title-song'>{title}</p>
-//             <p className='artis-song'>{artist}</p>
-//         </div> 
-//         <a className='btn btn-blue' href={urlspotify}>click</a>
-//     </div>
-//   )
-// }
-
-// export default musicbox
